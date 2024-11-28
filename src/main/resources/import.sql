08cf768a1bffafb1849af36da51e7d2dfc39ba15
Jordan Santos <jrdnsantos@gmail.com>
2024-11-28 07:23:46 -0300

    3.16. Desafio: Lombok e repositório de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('Lasai', 6.99);
+insert into restaurante (nome, taxa_frete) values ('Oteque', 5.99);
